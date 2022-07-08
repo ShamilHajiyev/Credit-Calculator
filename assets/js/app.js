9e82a84fd558412b4cef3d57cd6e8f1977df1556
ShamilHajiyev <shamilnh@code.edu.az>
2022-07-08 07:59:56 +0400

    Updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,17 +5,27 @@ var time = document.getElementById('time');
 var percentInput = document.getElementById('percent-input');
 var percent = document.getElementById('percent');
 
-var monthly;
-var total;
+var monthly = document.getElementById('monthly');
+var total = document.getElementById('total');
 
-loanInput.oninput = function ShowLoan() {
-    loan.innerHTML = loanInput.value;
+loan.innerHTML = loanInput.value + ' AZN';
+time.innerHTML = timeInput.value + ' ay';
+percent.innerHTML = percentInput.value + ' %';
+
+function Show(value, input, suffix) {
+    value.innerHTML = input.value + ' ' + suffix;
+    monthly.innerHTML = Math.round(loanInput.value * percentInput.value / 1200 / (1 - (Math.pow(1 / (1 + percentInput.value / 1200), timeInput.value))) * 100) / 100 + ' AZN';
+    total.innerHTML = (Math.round(loanInput.value * percentInput.value / 1200 / (1 - (Math.pow(1 / (1 + percentInput.value / 1200), timeInput.value))) * 100) / 100 * timeInput.value).toFixed(2) + ' AZN';
 }
 
-timeInput.oninput = function ShowTime() {
-    time.innerHTML = timeInput.value;
+function Calculate() {
+    Show(loan, loanInput, 'AZN')
+    Show(time, timeInput, 'ay')
+    Show(percent, percentInput, '%')
 }
 
-percentInput.oninput = function ShowPercent() {
-    percent.innerHTML = percentInput.value;
-}
+loanInput.oninput = function () { Calculate() };
+timeInput.oninput = function () { Calculate() };
+percentInput.oninput = function () { Calculate() };
+
+addEventListener(onload, Show(loan, loanInput, 'AZN'))
